3d0c4751be5041701974e40a779e3915a6d2da42
eugene <jurgenporubenger@gmail.com>
2020-07-29 07:26:23 +0300

    first

---
+++ src/client/App.tsx
@@ -0,0 +1,14 @@
+import * as React from 'react';
+import {render} from 'react-dom';
+
+const App = () => {
+    const [text,setText]=React.useState('');
+    React.useEffect(()=>{
+        fetch('api/hello')
+          .then(res=>res.json())
+          .then(res=>setText(res.text))
+    },[])
+
+    return <h1>{text}</h1>
+}
+render(<App/>, document.getElementById('app'));
